ecfb56d52884ebc1392f16bc14ac8996cc723712
yuki-0211 <yoshizumi.kf@om.asahi-kasei.co.jp>
2022-02-11 15:14:16 +0900

    bug-fix: fix init data

--- src/init/initInputRecord.ts
+++ src/init/initInputRecord.ts
@@ -1,7 +1,6 @@
 export const initInputRecord = () => {
   return [
-    { id: '0', value: 'English' },
-    { id: '1', value: 'Management' },
-    { id: '2', value: 'Science' },
+    { id: '0', value: 'test' },
+    { id: '1', value: 'test' },
   ];
 };
