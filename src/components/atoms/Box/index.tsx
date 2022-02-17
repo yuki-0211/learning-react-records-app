0227469a938e20d6d8c8ec4df4220a0480b5e1d8
yuki-0211 <yoshizumi.kf@om.asahi-kasei.co.jp>
2022-02-17 20:53:38 +0900

    feature: add box theme

--- src/components/atoms/Box/index.tsx
+++ src/components/atoms/Box/index.tsx
@@ -1,6 +1,6 @@
 import { Box as MuiBox, BoxProps as MuiBoxProps } from '@mui/system';
 
-interface BoxProps extends MuiBoxProps {}
+export interface BoxProps extends MuiBoxProps {}
 
 export const Box: React.VFC<BoxProps> = (props) => {
   return <MuiBox {...props} />;
